0fe021a988764e75f66dfd9f4312200054f550bf
disha rajput <dishacsevira@gmail.com>
2025-08-29 16:50:45 +0530

    Create server giving then path

---
+++ src/index.js
@@ -0,0 +1,29 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import path from "path";
+
+import productRoutes from "./routes/productRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
+import authRoutes from "./routes/authRoutes.js";
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.use("/product", productRoutes);
+app.use("/user", userRoutes);
+app.use("/auth", authRoutes);
+
+app.use("/uploads", express.static(path.join(process.cwd(), "uploads")));
+
+mongoose
+  .connect(process.env.MONGODB_URI, { dbName: "ecommerce-products" })
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log(`App sever is listen on PORT ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("Server not connected", err);
+  });
