5327cf6a40f7bf6a0058f41bab58f9fe82a212ed
disha rajput <dishacsevira@gmail.com>
2025-09-03 10:15:39 +0530

    giving validation in product schema

--- src/models/ProductSchema.js
+++ src/models/ProductSchema.js
@@ -9,7 +9,7 @@ const productSchema = new mongoose.Schema(
       minLength: [3, "Minimum length must have 3 characters"],
       maxLength: [100, "Maximum length must have 100 Characters"],
       match: [
-        /^[a-zA-Z0-9\s.()+]+$/,
+        /^[a-zA-Z0-9\s.()+-]+$/,
         "Name must not contain some special characters",
       ],
     },
@@ -25,6 +25,10 @@ const productSchema = new mongoose.Schema(
       required: [true, "Description is required"],
       minLength: [10, "Description must be at least 10 characters long"],
       maxLength: [500, "Description must be less than 500 characters"],
+      match: [
+        /^[a-zA-Z0-9\s.()+-]+$/,
+        "Name must not contain some special characters",
+      ],
     },
     brand: {
       type: String,
