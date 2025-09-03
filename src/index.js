7fe22c826f7e0112baf5788e85cefc239cd27899
disha rajput <dishacsevira@gmail.com>
2025-09-03 16:35:14 +0530

    improve code

--- src/index.js
+++ src/index.js
@@ -7,9 +7,16 @@ import productRoutes from "./routes/productRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import authRoutes from "./routes/authRoutes.js";
 
+const MONGODB_URI = process.env.MONGODB_URI;
+const PORT = process.env.PORT || 5000;
 const app = express();
 app.use(express.json());
 app.use(cors());
+
+mongoose.connect(MONGODB_URI, { dbName: "ecommerce-products" });
+mongoose.connection.on("connected", () => console.log("MongoDB Connected"));
+
+app.get("/", (req, res) => res.send("Ok"));
 
 app.use("/user", userRoutes);
 app.use("/products", productRoutes);
@@ -17,13 +24,4 @@ app.use("/auth", authRoutes);
 
 app.use("/uploads", express.static(path.join(process.cwd(), "uploads")));
 
-mongoose
-  .connect(process.env.MONGODB_URI, { dbName: "ecommerce-products" })
-  .then(() => {
-    app.listen(process.env.PORT, () => {
-      console.log(`App sever is listen on PORT ${process.env.PORT}`);
-    });
-  })
-  .catch((err) => {
-    console.log("Server not connected", err);
-  });
+app.listen(PORT, () => console.log(`App is listening on port ${PORT}`));
