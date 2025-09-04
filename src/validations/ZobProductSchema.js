4651891cff2d78bb32c0750ed15567cc5f84f728
disha rajput <dishacsevira@gmail.com>
2025-09-04 12:07:40 +0530

    add more brand and created by

--- src/validations/ZobProductSchema.js
+++ src/validations/ZobProductSchema.js
@@ -16,17 +16,35 @@ export const zodSchema = z.object(
     category: z.enum(["Laptop", "Mobile", "Tablet", "Accessory"], {
       errorMap: () => ({ message: "Invalid category" }),
     }),
-    brand: z.enum(["Apple", "Samsung", "Dell", "HP"], {
-      errorMap: () => ({
-        message: "Brand must be Apple, Samsung, Dell, or HP",
-      }),
-    }),
+    brand: z.enum(
+      [
+        "Apple",
+        "Samsung",
+        "Dell",
+        "HP",
+        "Google",
+        "OnePlus",
+        "Sony",
+        "Lenovo",
+        "Microsoft",
+        "Huawei",
+        "Xiaomi",
+        "Amazon",
+        "Vivo",
+      ],
+      {
+        errorMap: () => ({
+          message: "Brand must be Apple, Samsung, Dell, or HP",
+        }),
+      }
+    ),
 
     status: z.enum(["active", "inactive", "out_of_stock"]).default("active"),
 
     images: z.array(z.string()).optional(),
     stock: z.number().positive("Price must be greater than 0"),
   },
+  { createdBy: z.string() },
   {
     error: "Invalid data in body.",
   }
